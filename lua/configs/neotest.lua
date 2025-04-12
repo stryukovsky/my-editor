5e2f3684d8563199de7f9f2a1b6966183dc00407
Dmitry Stryukovsky <strukovsky1@gmail.com>
2025-04-12 15:55:44 +0300

    chore: fix scala test launch

--- lua/configs/neotest.lua
+++ lua/configs/neotest.lua
@@ -6,7 +6,7 @@ require("neotest").setup {
     require "neotest-scala" {
       -- Command line arguments for runner
       -- Can also be a function to return dynamic values
-      args = { "--no-color" },
+      args = { },
       -- Runner to use. Will use bloop by default.
       -- Can be a function to return dynamic value.
       -- For backwards compatibility, it also tries to read the vim-test scala config.
