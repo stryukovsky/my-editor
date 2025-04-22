dd4c56bdb14146bd71e09bd7adbc947c71fe0e68
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-04-22 10:26:05 +0300

    chore: add instructions on fedora

--- setup.fedora.sh
+++ setup.fedora.sh
@@ -10,7 +10,7 @@ yum install bzip2-devel
 dnf install openssl-devel
 dnf install libsqlite3x-devel
 dnf install golang
-
+dnf install neovim 
 
 
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh" | bash
@@ -24,7 +24,7 @@ curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz
 chmod 700 $HOME/Tools/cs
 cat 'export $PATH="$HOME/Tools/:$PATH"' > $HOME/.bashrc
 
-
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rustfmt
 
 curl -s "https://get.sdkman.io" | bash
@@ -34,4 +34,4 @@ pyenv install $PYTHON_VERSION
 pyenv global $PYTHON_VERSION 
 
 git clone https://github.com/stryukovsky/my-editor ~/.config/nvim/
- 
+nvim 
