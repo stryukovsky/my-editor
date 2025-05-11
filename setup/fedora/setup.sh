44024746f8db35a2d20f332297003392d20dad19
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-05-11 10:41:38 +0300

    chore: improve config

--- setup/fedora/setup.sh
+++ setup/fedora/setup.sh
@@ -1,8 +1,5 @@
 dnf install libubsan libasan lcov fzf zsh neovim  golang zlib-devel ripgrep gcc-c++ bpytop make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2 wireguard-tools postgresql-server postgresql-contrib alacritty tmux -y
 
-dnf copr enable varlad/zellij 
-dnf install zellij
-
 sudo chown -R $USER /usr/local/lib/
 sudo chown -R $USER /usr/local/bin/
 
