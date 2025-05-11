061d99a30285dba42b30b65e8d1865d3fe0c80f2
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-05-11 12:40:53 +0300

    fix: use better one

--- setup/fedora/gnome.sh
+++ setup/fedora/gnome.sh
@@ -1,3 +1,3 @@
 gsettings set org.gnome.mutter overlay-key ''
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
-gnome-extensions list --enabled | xargs -n1 gnome-extensions disable
+gsettings set org.gnome.shell enabled-extensions "[]"
