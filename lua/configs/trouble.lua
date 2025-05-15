d715a449ecaa3004f89ade51621ecd7cc1da4efc
Dmitry Stryukovsky <strukovsky1@gmail.com>
2025-05-15 18:34:51 +0300

    chore: trouble config minimum

--- lua/configs/trouble.lua
+++ lua/configs/trouble.lua
@@ -1,54 +1,16 @@
 local trouble = require "trouble"
 
----@class trouble.Mode: trouble.Config,trouble.Section.spec
-
----@class trouble.Config
----@field config? fun(opts:trouble.Config)
-trouble.setup({
-  debug = false,
-  auto_close = false, -- auto close when there are no items
-  auto_open = false, -- auto open when there are items
-  auto_preview = true, -- automatically open preview when on an item
-  auto_refresh = true, -- auto refresh when open
-  auto_jump = false, -- auto jump to the item when there's only one
-  focus = false, -- Focus the window when opened
-  restore = true, -- restores the last location in the list when opening
-  follow = true, -- Follow the current item
-  indent_guides = true, -- show indent guides
-  max_items = 200, -- limit number of items that can be displayed per section
-  multiline = true, -- render multi-line messages
-  pinned = false, -- When pinned, the opened trouble window will be bound to the current buffer
+trouble.setup {
+  symbols = {
+    multiline = false,
+  },
   warn_no_results = false, -- show a warning when there are no results
-  open_no_results = true, -- open the trouble window when there are no results
-  ---@type trouble.Window.opts
-  win = {}, -- window options for the results window. Can be a split or a floating window.
-  -- Window options for the preview window. Can be a split, floating window,
-  -- or `main` to show the preview in the main editor window.
-  ---@type trouble.Window.opts
-  preview = {
-    type = "main",
-    -- when a buffer is not yet loaded, the preview window will be created
-    -- in a scratch buffer with only syntax highlighting enabled.
-    -- Set to false, if you want the preview to always be a real loaded buffer.
-    scratch = true,
-  },
-  -- Throttle/Debounce settings. Should usually not be changed.
-  ---@type table<string, number|{ms:number, debounce?:boolean}>
-  throttle = {
-    refresh = 20, -- fetches new data when needed
-    update = 10, -- updates the window
-    render = 10, -- renders the window
-    follow = 100, -- follows the current item
-    preview = { ms = 100, debounce = true }, -- shows the preview for the current item
-  },
-  -- Key mappings can be set to the name of a builtin action,
-  -- or you can define your own custom action.
-  ---@type table<string, trouble.Action.spec|false>
+  open_no_results = false, -- open the trouble window when there are no results
   keys = {
     ["?"] = "help",
     r = "refresh",
     R = "toggle_refresh",
-    ["<Esc>"] = "close",
+    q = "close",
     o = "jump_close",
     ["<esc>"] = "cancel",
     ["<cr>"] = "jump",
@@ -102,100 +64,4 @@ trouble.setup({
       desc = "Toggle Severity Filter",
     },
   },
-  ---@type table<string, trouble.Mode>
-  modes = {
-    -- sources define their own modes, which you can use directly,
-    -- or override like in the example below
-    lsp_references = {
-      -- some modes are configurable, see the source code for more details
-      params = {
-        include_declaration = true,
-      },
-    },
-    -- The LSP base mode for:
-    -- * lsp_definitions, lsp_references, lsp_implementations
-    -- * lsp_type_definitions, lsp_declarations, lsp_command
-    lsp_base = {
-      params = {
-        -- don't include the current location in the results
-        include_current = false,
-      },
-    },
-    -- more advanced example that extends the lsp_document_symbols
-    symbols = {
-      desc = "document symbols",
-      mode = "lsp_document_symbols",
-      multiline = false,
-      focus = false,
-      win = { position = "right" },
-      filter = {
-        -- remove Package since luals uses it for control flow structures
-        ["not"] = { ft = "lua", kind = "Package" },
-        any = {
-          -- all symbol kinds for help / markdown files
-          -- ft = { "help", "markdown" },
-          -- default set of symbol kinds
-          kind = {
-            "Class",
-            "Constructor",
-            "Enum",
-            "Field",
-            "Function",
-            "Interface",
-            "Method",
-            "Module",
-            "Namespace",
-            "Package",
-            "Property",
-            "Struct",
-            "Trait",
-          },
-        },
-      },
-    },
-  },
-  -- stylua: ignore
-  icons = {
-    ---@type trouble.Indent.symbols
-    indent = {
-      top           = "│ ",
-      middle        = "├╴",
-      last          = "└╴",
-      -- last          = "-╴",
-      -- last       = "╰╴", -- rounded
-      fold_open     = " ",
-      fold_closed   = " ",
-      ws            = "  ",
-    },
-    folder_closed   = " ",
-    folder_open     = " ",
-    kinds = {
-      Array         = " ",
-      Boolean       = "󰨙 ",
-      Class         = " ",
-      Constant      = "󰏿 ",
-      Constructor   = " ",
-      Enum          = " ",
-      EnumMember    = " ",
-      Event         = " ",
-      Field         = " ",
-      File          = " ",
-      Function      = "󰊕 ",
-      Interface     = " ",
-      Key           = " ",
-      Method        = "󰊕 ",
-      Module        = " ",
-      Namespace     = "󰦮 ",
-      Null          = " ",
-      Number        = "󰎠 ",
-      Object        = " ",
-      Operator      = " ",
-      Package       = " ",
-      Property      = " ",
-      String        = " ",
-      Struct        = "󰆼 ",
-      TypeParameter = " ",
-      Variable      = "󰀫 ",
-    },
-  },
-})
+}
