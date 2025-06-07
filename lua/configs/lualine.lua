217971696e09f65c89dc3264b87c33b6210a00f1
Dmitry Stryukovsky <strukovsky1@gmail.com>
2025-06-07 11:54:23 +0300

    chore: improve

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -1,15 +1,3 @@
-local trouble = require "trouble"
-local trouble_symbols = trouble.statusline {
-  mode = "lsp_document_symbols",
-  groups = {},
-  title = false,
-  filter = { range = true },
-  format = "{kind_icon}{symbol.name:Normal}",
-  -- The following line is needed to fix the background color
-  -- Set it to the lualine section you want to use
-  hl_group = "lualine_c_normal",
-}
-
 require("lualine").setup {
   options = {
     icons_enabled = true,
@@ -52,10 +40,6 @@ require("lualine").setup {
           unnamed = "[No Name]", -- Text to show for unnamed buffers.
           newfile = "[New]", -- Text to show for newly created file before first write
         },
-      },
-      {
-        trouble_symbols.get,
-        cond = trouble_symbols.has,
       },
     },
 
