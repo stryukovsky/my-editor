5867a568f0126b4fe72e7ebbe61a5bf4798f060e
Dmitry Stryukovsky <strukovsky1@gmail.com>
2025-06-10 16:36:55 +0300

    chore: remove ui obsolete

--- lua/ui/new_branch.lua
+++
@@ -1,8 +0,0 @@
-local create_float_input = require "ui.float_input"
-
-return function()
-  create_float_input("new branch", "", function(value)
-    local response = vim.fn.system("git checkout -b " .. value)
-    vim.print(response)
-  end)
-end
