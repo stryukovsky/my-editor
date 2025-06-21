64927fa9c2ce34af1586fe456da9065433af16b0
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-06-21 20:06:06 +0300

    chore(livegrep): improve results output

--- lua/mappings/telescope/live_grep.lua
+++ lua/mappings/telescope/live_grep.lua
@@ -1,13 +1,22 @@
 local open_with_trouble = require("trouble.sources.telescope").open
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
 return {
   n = {
     ["<cr>"] = function(bufnr, opts)
-      open_with_trouble(bufnr, opts)
+      local picker = action_state.get_current_picker(prompt_bufnr)
+      open_with_trouble(bufnr, { focus = true })
+      vim.schedule(function()
+        actions.select_default(bufnr, opts)
+      end)
     end,
   },
   i = {
     ["<cr>"] = function(bufnr, opts)
-      open_with_trouble(bufnr, opts)
+      open_with_trouble(bufnr, { focus = true, mode = "telescope_files" })
+      -- vim.schedule(function()
+      --   actions.select_default(bufnr, opts)
+      -- end)
     end,
   },
 }
