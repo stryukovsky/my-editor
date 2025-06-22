3d508f4dc9af62d6eeafed1b6facaf8a45407903
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-06-22 11:52:15 +0300

    chore(live_grep): add timeout

--- lua/mappings/telescope/live_grep.lua
+++ lua/mappings/telescope/live_grep.lua
@@ -5,10 +5,10 @@ local function open_with_trouble(bufnr)
   local selection = action_state.get_selected_entry()
 ---@diagnostic disable-next-line: missing-fields
   trouble.open(bufnr, { focus = false, mode = "telescope_files" })
-  vim.schedule(function()
+  vim.defer_fn(function()
     vim.cmd("edit! " .. selection.path)
     vim.api.nvim_win_set_cursor(0, {selection.lnum, selection.col})
-  end)
+  end, 100)
 end
 
 return {
