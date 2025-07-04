853d0c3ae390ce391b9c915ec698720ca5756c30
Dmitry Stryukovsky <strukovsky1@gmail.com>
2025-07-05 00:22:49 +0300

    chore(llm): compat

--- lua/configs/llm.lua
+++ lua/configs/llm.lua
@@ -27,176 +27,184 @@ local keys_close = { "<esc>", "q", "Q", "<A-q>", "<A-a>", "<A-s>", "<A-w>", "<A-
 local keys_accept = { "<C-s>", "<leader>y", "<cr>" }
 local keys_reject = { "<C-x>", "<C-c>" }
 
-vim.env.LLM_KEY = ""
-local llm = require "llm"
-local tools = require "llm.tools"
-llm.setup {
-  url = "http://localhost:11434/api/chat",
-  api_type = "ollama",
-  temperature = 0.3,
-  top_p = 0.7,
-  model = "qwen2.5-coder:7b",
-  streaming_handler = local_llm_streaming_handler,
-  app_handler = {
-    Completion = {
-      style = "virtual_text",
-      handler = tools.completion_handler,
-      opts = {
-        -------------------------------------------------
-        ---                   ollama
-        -------------------------------------------------
-        url = "http://localhost:11434/v1/completions",
-        model = "qwen2.5-coder:7b",
-        api_type = "ollama",
+local function is_ollama_installed()
+  return pcall(function()
+    vim.system({ "ollama" }):wait()
+  end)
+end
 
-        n_completions = 1,
-        context_window = 142,
-        max_tokens = 20,
+if is_ollama_installed() then
+  vim.env.LLM_KEY = ""
+  local llm = require "llm"
+  local tools = require "llm.tools"
+  local model = "qwen2.5-coder:7b"
+  vim.print("Ollama installed, setup using " .. model)
+  llm.setup {
+    url = "http://localhost:11434/api/chat",
+    api_type = "ollama",
+    temperature = 0.3,
+    top_p = 0.7,
+    model = model,
+    streaming_handler = local_llm_streaming_handler,
+    app_handler = {
+      Completion = {
+        style = "virtual_text",
+        handler = tools.completion_handler,
+        opts = {
+          -------------------------------------------------
+          ---                   ollama
+          -------------------------------------------------
+          url = "http://localhost:11434/v1/completions",
+          api_type = "ollama",
 
-        -- A mapping of filetype to true or false, to enable completion.
-        filetypes = {
-          sh = true,
-          rust = true, -- Note. To prevent this from happening, use ! as prefix before the command!!
-          vimrc = true,
-          config = true,
-          jsonnet = true,
-          html = true,
-          javascript = true,
-          python = true,
-          bash = true,
-          julia = true,
-          typescript = true,
-          scala = true,
-          go = true,
-          yaml = true,
-          json = true,
-          kotlin = true,
-          java = true,
-          lua = true,
-          php = true,
-        },
+          n_completions = 1,
+          context_window = 142,
+          max_tokens = 20,
 
-        -- Whether to enable completion of not for filetypes not specifically listed above.
-        default_filetype_enabled = false,
+          -- A mapping of filetype to true or false, to enable completion.
+          filetypes = {
+            sh = true,
+            rust = true, -- Note. To prevent this from happening, use ! as prefix before the command!!
+            vimrc = true,
+            config = true,
+            jsonnet = true,
+            html = true,
+            javascript = true,
+            python = true,
+            bash = true,
+            julia = true,
+            typescript = true,
+            scala = true,
+            go = true,
+            yaml = true,
+            json = true,
+            kotlin = true,
+            java = true,
+            lua = true,
+            php = true,
+          },
 
-        auto_trigger = true,
+          -- Whether to enable completion of not for filetypes not specifically listed above.
+          default_filetype_enabled = false,
 
-        -- just trigger by { "@", ".", "(", "[", ":", " " } for `style = "nvim-cmp"`
-        only_trigger_by_keywords = false,
+          auto_trigger = true,
 
-        style = "virtual_text", -- nvim-cmp or blink.cmp
+          -- just trigger by { "@", ".", "(", "[", ":", " " } for `style = "nvim-cmp"`
+          only_trigger_by_keywords = false,
 
-        timeout = 10, -- max request time
+          style = "virtual_text", -- nvim-cmp or blink.cmp
 
-        -- only send the request every x milliseconds, use 0 to disable throttle.
-        throttle = 1000,
-        -- debounce the request in x milliseconds, set to 0 to disable debounce
-        debounce = 400,
+          timeout = 10, -- max request time
 
-        --------------------------------
-        ---   just for virtual_text
-        --------------------------------
-        keymap = {
-          virtual_text = {
-            accept = {
-              mode = "i",
-              keys = "<C-s>",
+          -- only send the request every x milliseconds, use 0 to disable throttle.
+          throttle = 1000,
+          -- debounce the request in x milliseconds, set to 0 to disable debounce
+          debounce = 400,
+
+          --------------------------------
+          ---   just for virtual_text
+          --------------------------------
+          keymap = {
+            virtual_text = {
+              accept = {
+                mode = "i",
+                keys = "<C-s>",
+              },
             },
           },
         },
       },
-    },
-    WordTranslate = {
-      handler = tools.flexi_handler,
-      prompt = "Translate the following text to Russian, please only return the translation",
-      opts = {
-        parse_handler = local_llm_parse_handler,
-        exit_on_move = true,
-        enter_flexible_window = false,
-      },
-    },
-    Ask = {
-      handler = tools.disposable_ask_handler,
-      opts = {
-        position = {
-          row = 2,
-          col = 0,
+      WordTranslate = {
+        handler = tools.flexi_handler,
+        prompt = "Translate the following text to Russian, please only return the translation",
+        opts = {
+          parse_handler = local_llm_parse_handler,
+          exit_on_move = true,
+          enter_flexible_window = false,
         },
-        title = " Ask ",
-        inline_assistant = true,
+      },
+      Ask = {
+        handler = tools.disposable_ask_handler,
+        opts = {
+          position = {
+            row = 2,
+            col = 0,
+          },
+          title = " Ask ",
+          inline_assistant = true,
 
-        -- Whether to use the current buffer as context without selecting any text (the tool is called in normal mode)
-        enable_buffer_context = true,
-        language = "English",
-        -- url = "http://localhost:11434/api/chat",
-        -- api_type = "ollama",
-        -- parse_handler = local_llm_parse_handler,
-        exit_on_move = true,
-        enter_flexible_window = false,
-        -- display diff
-        display = {
-          mapping = {
-            mode = "n",
-            keys = { "<cr>" },
+          -- Whether to use the current buffer as context without selecting any text (the tool is called in normal mode)
+          enable_buffer_context = true,
+          language = "English",
+          -- url = "http://localhost:11434/api/chat",
+          -- api_type = "ollama",
+          -- parse_handler = local_llm_parse_handler,
+          exit_on_move = true,
+          enter_flexible_window = false,
+          -- display diff
+          display = {
+            mapping = {
+              mode = "n",
+              keys = { "<cr>" },
+            },
+            action = nil,
           },
-          action = nil,
-        },
-        accept = {
-          mapping = {
-            mode = "n",
-            keys = keys_accept,
+          accept = {
+            mapping = {
+              mode = "n",
+              keys = keys_accept,
+            },
+            action = nil,
           },
-          action = nil,
-        },
-        reject = {
-          mapping = {
-            mode = "n",
-            keys = keys_reject,
+          reject = {
+            mapping = {
+              mode = "n",
+              keys = keys_reject,
+            },
+            action = nil,
           },
-          action = nil,
-        },
-        close = {
-          mapping = {
-            mode = "n",
-            keys = keys_close,
+          close = {
+            mapping = {
+              mode = "n",
+              keys = keys_close,
+            },
+            action = nil,
           },
-          action = nil,
         },
       },
-    },
-    TestCode = {
-      handler = tools.side_by_side_handler,
-      prompt = [[ Write some test cases for the following code, only return the test cases.
+      TestCode = {
+        handler = tools.side_by_side_handler,
+        prompt = [[ Write some test cases for the following code, only return the test cases.
 Give the code content directly, do not use code blocks or other tags to wrap it. ]],
-      opts = {
-        right = {
-          title = " Test Cases ",
-        },
-        accept = {
-          mapping = {
-            mode = "n",
-            keys = keys_accept,
+        opts = {
+          right = {
+            title = " Test Cases ",
           },
-          action = nil,
-        },
-        reject = {
-          mapping = {
-            mode = "n",
-            keys = keys_reject,
+          accept = {
+            mapping = {
+              mode = "n",
+              keys = keys_accept,
+            },
+            action = nil,
           },
-          action = nil,
-        },
-        close = {
-          mapping = {
-            mode = "n",
-            keys = keys_close,
+          reject = {
+            mapping = {
+              mode = "n",
+              keys = keys_reject,
+            },
+            action = nil,
           },
-          action = nil,
+          close = {
+            mapping = {
+              mode = "n",
+              keys = keys_close,
+            },
+            action = nil,
+          },
         },
       },
-    },
-    DocString = {
-      prompt = [[ You are an AI programming assistant. You need to write a really good docstring that follows a best practice for the given language.
+      DocString = {
+        prompt = [[ You are an AI programming assistant. You need to write a really good docstring that follows a best practice for the given language.
 
 Your core tasks include:
 - parameter and return types (if applicable).
@@ -207,39 +215,39 @@ You must:
 - Follow the format of examples carefully if the examples are provided.
 - Use Markdown formatting in your answers.
 - Include the programming language name at the start of the Markdown code blocks.]],
-      handler = tools.action_handler,
-      opts = {
-        only_display_diff = true,
-        templates = {
-          lua = [[- For the Lua language, you should use the LDoc style.
+        handler = tools.action_handler,
+        opts = {
+          only_display_diff = true,
+          templates = {
+            lua = [[- For the Lua language, you should use the LDoc style.
 - Start all comment lines with "---".
 ]],
-          go = "use godoc format for documentation",
-        },
-        accept = {
-          mapping = {
-            mode = "n",
-            keys = keys_accept,
+            go = "use godoc format for documentation",
           },
-          action = nil,
-        },
-        reject = {
-          mapping = {
-            mode = "n",
-            keys = keys_reject,
+          accept = {
+            mapping = {
+              mode = "n",
+              keys = keys_accept,
+            },
+            action = nil,
           },
-          action = nil,
-        },
-        close = {
-          mapping = {
-            mode = "n",
-            keys = keys_close,
+          reject = {
+            mapping = {
+              mode = "n",
+              keys = keys_reject,
+            },
+            action = nil,
           },
-          action = nil,
+          close = {
+            mapping = {
+              mode = "n",
+              keys = keys_close,
+            },
+            action = nil,
+          },
         },
       },
     },
-  },
   -- stylua: ignore
   keys = {
     -- The keyboard mapping for the input window.
@@ -268,4 +276,5 @@ You must:
     ["JumpToTop"]         = { mode = "n", key = "gg" },
     ["JumpToBottom"]      = { mode = "n", key = "G" },
   },
-}
+  }
+end
