06c7f308d328d984dae577762348dd88b750b5e9
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-07-12 12:59:26 +0300

    chore(tmux): improve current dir output

--- lua/mappings/telescope/live_grep.lua
+++ lua/mappings/telescope/live_grep.lua
@@ -12,15 +12,35 @@ local function open_with_trouble(bufnr)
   end
   local count = picker.manager:num_results()
   if count > 1 then
+    -- this stuff disables sorting 
+    local sort_disabler = 0
+
     ---@diagnostic disable-next-line: missing-fields
-    trouble.open(bufnr, { focus = false, mode = "telescope_files" })
+    trouble.open(bufnr, {
+      focus = true,
+      mode = "telescope_files",
+      sorters = {},
+      sort = function(_)
+        -- function returns incrementing index of every item
+        -- so basically, it keeps the order of items from telescope
+        -- disabling of sorting helps better navigate from telescope choice 
+        -- to current item in trouble.nvim
+        sort_disabler = sort_disabler + 1
+        return sort_disabler
+      end,
+    })
+    local index = 2 + picker:get_selection_row() -- This gives you the 1-based index
+    vim.defer_fn(function()
+      vim.api.nvim_win_set_cursor(0, { index, 0 })
+    end, 200)
+  else
+    vim.defer_fn(function()
+      vim.cmd("edit! " .. selection.path)
+    end, 200)
+    vim.defer_fn(function()
+      vim.api.nvim_win_set_cursor(0, { selection.lnum, selection.col })
+    end, 250)
   end
-  vim.defer_fn(function()
-    vim.cmd("edit! " .. selection.path)
-  end, 200)
-  vim.defer_fn(function()
-    vim.api.nvim_win_set_cursor(0, { selection.lnum, selection.col })
-  end, 250)
 end
 
 return {
