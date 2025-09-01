51ff74a107156d7b1631f8142e8fe11163bbb579
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-09-01 12:28:18 +0300

    chore(pure): pure version

--- lua/configs/blink.lua
+++ lua/configs/blink.lua
@@ -42,7 +42,7 @@ require("blink-cmp").setup {
   -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
   --
   -- See the fuzzy documentation for more information
-  fuzzy = { implementation = "prefer_rust_with_warning" },
+  fuzzy = { implementation = "lua" },
 
   completion = {
     documentation = { auto_show = true, window = { border = "single" } },
