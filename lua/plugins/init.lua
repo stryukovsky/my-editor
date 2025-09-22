72646295e73e255484764974e5e9145d3457c266
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-09-22 12:36:41 +0300

    chore(LuaSnip): remove obsolete

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,14 +31,6 @@ return {
   {
     "nvim-tree/nvim-web-devicons",
   },
-  {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    dependencies = { "rafamadriz/friendly-snippets" },
-    build = "make install_jsregexp",
-  },
   -- {
   --   "hrsh7th/nvim-cmp",
   --   event = "InsertEnter",
