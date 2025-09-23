e981aea7a70fd80d986ff7d2d13e0657bfd357e8
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-09-23 23:37:11 +0300

    chore(metals): add env for metals jvm opts

--- lua/configs/scalametals.lua
+++ lua/configs/scalametals.lua
@@ -1,10 +1,40 @@
+local function parse_java_opts(opts_string)
+  local result = {}
+
+  -- Handle empty or nil input
+  if not opts_string or opts_string == "" then
+    return result
+  end
+
+  -- Split by spaces first to get individual options
+  for opt in opts_string:gmatch "%S+" do
+    -- Check if the option contains '='
+    local key, value = opt:match "^(.-)=(.*)$"
+
+    if key and value then
+      -- Option has key=value format
+      table.insert(result, key)
+      table.insert(result, value)
+    else
+      -- Option doesn't have '=' (standalone option)
+      table.insert(result, opt)
+    end
+  end
+
+  return result
+end
+
 local function create_config()
   local metals_config = require("metals").bare_config()
 
-  -- Example of settings
+  local success, server_props = pcall(parse_java_opts, vim.fn.getenv "METALS_JAVA_OPTS")
   metals_config.settings = {
     showImplicitArguments = true,
-    excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },}
+    excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+  }
+  if server_props and success then
+    metals_config.settings.serverProperties = server_props
+  end
   metals_config.init_options.statusBarProvider = "off"
 
   -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
@@ -17,13 +47,13 @@ local function create_config()
   return metals_config
 end
 
-return function (self, _)
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = self.ft,
-        callback = function()
-          require("metals").initialize_or_attach(create_config())
-        end,
-        group = nvim_metals_group,
-      })
+return function(self, _)
+  local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = self.ft,
+    callback = function()
+      require("metals").initialize_or_attach(create_config())
+    end,
+    group = nvim_metals_group,
+  })
 end
