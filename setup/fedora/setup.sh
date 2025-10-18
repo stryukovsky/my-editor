84aeb44979129523722e7a2d876d3bfb7a427c84
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2025-10-18 10:22:16 +0300

    chore(fedora): add lib uchardet-devel

--- setup/fedora/setup.sh
+++ setup/fedora/setup.sh
@@ -1,5 +1,5 @@
 dnf copr enable dejan/lazygit -y
-dnf install libubsan libasan lcov fzf zsh neovim  golang zlib-devel ripgrep gcc-c++ bpytop make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2 wireguard-tools postgresql-server postgresql-contrib alacritty tmux  wl-clipboard sassc gtk-murrine-engine gnome-themes-extra  gnome-extensions-app fd-find lazygit libpq-devel -y
+dnf install libubsan libasan lcov fzf zsh neovim  golang zlib-devel ripgrep gcc-c++ bpytop make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2 wireguard-tools postgresql-server postgresql-contrib alacritty tmux  wl-clipboard sassc gtk-murrine-engine gnome-themes-extra  gnome-extensions-app fd-find lazygit libpq-devel uchardet-devel -y
 
 sudo chown -R $USER /usr/local/lib/
 sudo chown -R $USER /usr/local/bin/
