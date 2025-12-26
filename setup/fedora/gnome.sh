02e9f60559c46d893501d12d3bc9c55a93d413f5
Dmitry Stryukovsky <strukovsky1@gmail.com>
2025-12-26 17:48:43 +0300

    chore(gnome): add dash to dock stuff

--- setup/fedora/gnome.sh
+++ setup/fedora/gnome.sh
@@ -2,3 +2,6 @@ gsettings set org.gnome.mutter overlay-key ''
 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
 gsettings set org.gnome.desktop.input-sources xkb-options "['altwin:ctrl_rwin']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4','<Super>q']"
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ $schema_dash_to_dock set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DOTS'
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ $schema_dash_to_dock set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 16
+gsettings --schemadir ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas/ $schema_dash_to_dock set org.gnome.shell.extensions.dash-to-dock intellihide false
