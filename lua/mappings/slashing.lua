3d0c537a82ce6aadbb3c54965a35f54a7937c34e
Dmitry Stryukovsky <strukovsky1@gmail.com>
2026-02-05 00:36:40 +0300

    chore(slashing): add operator ux

---
+++ lua/mappings/slashing.lua
@@ -0,0 +1,17 @@
+local map = require("mappings.map")
+map("v", "/", function()
+  vim.cmd "nohlsearch"
+  -- send esc key so selection will be handled properly
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "x", false)
+  vim.fn.feedkeys("/\\%V", "n")
+end, { desc = "Search in visual selection" })
+
+map("n", "<leader>rr", function()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":%s///g<Left><Left>", true, false, true), "n", false)
+  vim.cmd "nohlsearch"
+end, { desc = "Substitute in entire file" })
+
+map("n", "<leader>ri", function()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("gv:s///g<Left><Left>", true, false, true), "n", false)
+  vim.cmd "nohlsearch"
+end, { desc = "Substitute in selection" })
