488227a2d0058ffdb088069a95007e8212cc15b8
Dmitry Stryukovsky <strukovsky1@gmail.com>
2026-02-05 19:39:10 +0300

    chore(misc): aidviser stuff

--- lua/configs/aidviser.lua
+++ lua/configs/aidviser.lua
@@ -1,2 +1,3 @@
-local aidviser = require("plugin_name")
-aidviser.setup()
+local aidviser = require "plugin_name"
+aidviser.setup
+ { model = "moonshotai/kimi-k2.5", endpoint = "https://openrouter.ai/api/v1/chat/completions", provider = "openai", credential_env_name = "OPENROUTER_API_KEY" }
