c088660f79b8af1f43091e30dfc52d0b0a271bf4
Dmitry Stryukovsky <strukovsky1@gmail.com>
2026-03-06 00:43:39 +0300

    chore(lspconfig): solidity_ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -13,6 +13,15 @@ local lsp_with_default_conf = {
   "jdtls",
 }
 
+vim.lsp.config("solidity_ls", {
+  settings = {
+    solidity = {
+      enabledSolhint = false,
+      enabledSolium = false,
+    },
+  },
+})
+
 -- vim.lsp.config("*", {})
 for _, lsp_name in ipairs(lsp_with_default_conf) do
   vim.lsp.enable(lsp_name)
