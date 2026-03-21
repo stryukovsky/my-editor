87f976e021f901d26adbd988d479af5b20beee88
Dmitry Stryukovsky <stryukovsky1@gmail.com>
2026-03-21 11:09:06 +0300

    chore(current-buffer-fuzzy): wip

---
+++ lua/mappings/telescope/current_buffer_fuzzy.lua
@@ -0,0 +1,11 @@
+local wrap_telescope_action = require "mappings.telescope_action_wrapper"
+local trouble_integration = require "mappings.telescope.trouble_integration"
+
+return {
+  n = {
+    ["<cr>"] = wrap_telescope_action(trouble_integration "telescope_files"),
+  },
+  i = {
+    ["<cr>"] = wrap_telescope_action(trouble_integration "telescope_files"),
+  },
+}
