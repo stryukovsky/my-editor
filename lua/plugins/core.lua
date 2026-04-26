8cf005b3b579fb3d22a1cb48a3b2f4d2adffe22e
Dmitry Stryukovsky <strukovsky1@gmail.com>
2026-04-26 19:46:03 +0300

    chore(update): vast update of plugins probably bad

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -67,8 +67,7 @@ return {
   {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
+      "lewis6991/async.nvim",
     },
     lazy = false,
   },
