c532b137f07edbf5443703dd276ea0e788c0be10
Dmitry Stryukovsky <strukovsky1@gmail.com>
2026-07-19 09:08:10 +0300

    chore(gomove): no indent

--- lua/configs/gomove.lua
+++ lua/configs/gomove.lua
@@ -1,9 +1,8 @@
-
 require("gomove").setup {
   -- whether or not to map default key bindings, (true/false)
   map_defaults = false,
   -- whether or not to reindent lines moved vertically (true/false)
-  reindent = true,
+  reindent = false,
   -- whether or not to undo join same direction moves (true/false)
   undojoin = true,
   -- whether to not to move past end column when moving blocks horizontally, (true/false)
