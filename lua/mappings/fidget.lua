6d76320713da783c84b777d412216e561eb3f260
Dmitry Stryukovsky <strukovsky1@gmail.com>
2026-08-13 00:16:13 +0300

    chore(lsp_controls): add mapping to restart lsp

--- lua/mappings/fidget.lua
+++
@@ -1,2 +0,0 @@
-local map = require "mappings.map"
-map("n", "<leader>lsp", "<cmd>Telescope fidget<cr>", { desc = "LSP logs" })
---
+++ lua/mappings/lsp_controls.lua
@@ -0,0 +1,2 @@
+local map = require "mappings.map"
+map("n", "<leader>lsp", "<cmd>lsp restart<cr>", { desc = "Restart LSP" })
